3c0389d5f6bb324eada02cb8649f01bbdce9d551
Asunp <46518973+Asunp@users.noreply.github.com>
2019-02-04 10:22:50 +0100

    Update DateOne.java

--- src/DateOne.java
+++ src/DateOne.java
@@ -1,9 +1,9 @@
 
 public class DateOne {
 
-    int day = 0;
-    int month = 0;
-    int year = 0;
+    int day = 50;
+    int month = 50;
+    int year = 50;
     
     public static void main(String[] args) {
         
